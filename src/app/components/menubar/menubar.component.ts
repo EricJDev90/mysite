03fba50b78910fdc9f4d7f03a04a6f370913e917
Eric J <ericjdev90@gmail.com>
2024-11-11 20:37:57 -0500

    add mobile menu

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonMenu, IonMenuToggle, IonMenuButton, IonButtons, MenuController} from '@ionic/angular/standalone';
 import { GlobalStateService } from 'src/app/services/global-state.service';
 import { DataModel } from 'src/app/types/globalTypes';
 import * as icons from 'ionicons/icons';
@@ -11,14 +11,34 @@ import { addIcons } from 'ionicons';
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterOutlet, RouterLink, RouterLinkActive, IonIcon]
+  imports: [
+    CommonModule, 
+    IonHeader, 
+    IonToolbar,
+    IonTitle, 
+    IonContent, 
+    IonButton, 
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive, 
+    IonIcon, 
+    IonMenu, 
+    IonMenuToggle, 
+    IonMenuButton, 
+    IonButtons
+  ]
 })
 export class MenubarComponent  implements OnInit {
   private stateService = inject(GlobalStateService)
   data: DataModel | null = null;
+  private menu = inject(MenuController)
   
   ngOnInit() {
     addIcons({ ...icons });
     this.stateService.data.subscribe(update => this.data = update)
+  }
+
+  closeMenu() {
+    this.menu.close();
   }
 }
